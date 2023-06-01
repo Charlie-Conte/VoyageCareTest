6043ef61f5dd9e92f040c4fd25261aa019b467a6
Charlie.Conte <charlie.conte@outlook.com>
2023-06-01 19:33:32 +0100

    Added Scaffolding
    so i can overwrite the default ui

---
+++ StaffPortal.DataAccess/Staff/Details.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaffPortal.DataAccess.Staff
+{
+    public class Details
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
